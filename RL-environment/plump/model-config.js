33a24ef8d53fe468b2aa66d34d26288fc8b8b8fe
Max Rehman Linder <max.r.linder@hotmail.com>
2026-08-16 16:06:13 +0200

    Update Plump models to checkpoint 30100

--- RL-environment/plump/model-config.js
+++ RL-environment/plump/model-config.js
@@ -2,10 +2,10 @@
 export const PLUMP_MODEL_CONFIG = Object.freeze({
   actorPrecision: "fp32",
   actorManifests: Object.freeze({
-    fp32: "plump-ppo-28700-fp32.json",
-    fp16: "plump-ppo-28700-fp16.json",
+    fp32: "plump-ppo-30100-fp32.json",
+    fp16: "plump-ppo-30100-fp16.json",
   }),
-  oracleManifest: "plump-oracle-28700-fp32.json",
+  oracleManifest: "plump-oracle-30100-fp32.json",
   adminPrecisionQuery: "plump-model",
 });
 
