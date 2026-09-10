9a7a71440af825b8b5af0d0a9ec0886994d60d0c
Max Rehman Linder <max.r.linder@hotmail.com>
2026-09-10 13:12:50 +0200

    Update Plump model to checkpoint 97100

--- RL-environment/plump/game.js
+++ RL-environment/plump/game.js
@@ -2,7 +2,7 @@ import {
   BrowserPpoAgent,
   modelCardId,
   modelSuits,
-} from "./model-client.js?v=95200-ev1";
+} from "./model-client.js?v=97100-ev1";
 import {
   generateRoomCode,
   normalizeRoomCode,
