a9e6d02dceadd3da8fe20663c97f557ba48aeacb
Max Rehman Linder <max.r.linder@hotmail.com>
2026-09-13 17:39:27 +0200

    Update Plump model to checkpoint 100100

--- RL-environment/plump/model-client.js
+++ RL-environment/plump/model-client.js
@@ -2,7 +2,7 @@ import * as ort from "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.27.0/dist/o
 import {
   PLUMP_MODEL_CONFIG,
   configuredActorPrecision,
-} from "./model-config.js?v=99100-ev1";
+} from "./model-config.js?v=100100-ev1";
 import {
   MODEL_LIMITS,
   SUITS,
